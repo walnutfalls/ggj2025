67d0b133f84c67574b7005765ca1800588bd4244
ek <1350074+ek68794998@users.noreply.github.com>
2025-01-25 12:18:12 -0800

    Add counters for bubbles alive & popped

--- Assets/Scripts/BubbleDirector.cs
+++ Assets/Scripts/BubbleDirector.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class BubbleDirector : SingletonBase<BubbleDirector>
 {
+    public int BubblesAliveCount { get; private set; } = 0;
 
+    public int BubblesPoppedCount { get; private set; } = 0;
+
+    public void OnBubblePopped()
+    {
+        this.BubblesAliveCount--;
+        this.BubblesPoppedCount++;
+    }
+
+    public void OnBubbleSpawned()
+    {
+        this.BubblesAliveCount++;
+    }
+
+    public void ResetBubbleCounts()
+    {
+        this.BubblesAliveCount = 0;
+        this.BubblesPoppedCount = 0;
+    }
 }
