d40b2de6ead760b9ac5e9ba692085cd8d97c3be8
Jared Rowe <jared.w.rowe@gmail.com>
2025-01-25 11:33:50 -0800

    Audio System
    Added an audio system. This is based around creating Sound scriptables which hold sound clips and their settings. Sounds are held by an AudioLibrary scriptable, which the AudioSystem singleton will use to generate and play sounds from.

---
+++ Assets/Scripts/Audio/AudioLibrary.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "New Audio Library", menuName = "Audio/Audio Library")]
+public class AudioLibrary : ScriptableObject {
+    [Tooltip("List of sound objects that will hold and play audio clips with defined settings.")]
+    [SerializeField] private List<Sound> _sounds;
+
+    public List<Sound> Sounds { get => _sounds; }
+
+    public void PlaySound(string soundName) {
+        GetSound(soundName).PlaySound();
+    }
+
+    private Sound GetSound(string soundName) {
+        for (int i = 0; i < Sounds.Count; i++) {
+            if (Sounds[i].SoundName != soundName) {
+                continue;
+            }
+
+            return Sounds[i];
+        }
+
+        Debug.Log($"Could not find sound with provided name {soundName}!", this);
+        return null;
+    }
+}
