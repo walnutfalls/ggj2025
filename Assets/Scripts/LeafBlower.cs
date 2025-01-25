29926e12d930875d2875e1f306396b0e2ca77566
Jared Rowe <jared.w.rowe@gmail.com>
2025-01-25 14:14:15 -0800

    Leaf Blower inputs
    Added inputs for the leaf blower, as well as audio that plays when it's blowing.

--- Assets/Scripts/LeafBlower.cs
+++ Assets/Scripts/LeafBlower.cs
@@ -15,12 +15,47 @@ public class LeafBlower : MonoBehaviour {
     [Tooltip("Amount of force to apply per second to bubbles within the leaf blower cone.")]
     [SerializeField] private float _blowForce;
 
-    private void Update() {
-        for (int i = 0; i < _bubblePool.AllBubbles.Count; i++) {
-            if (IsWithinCone(_bubblePool.AllBubbles[i].transform.position)) {
-                _bubblePool.AllBubbles[i].Rigidbody.AddForce((_bubblePool.AllBubbles[i].transform.position - transform.position).normalized * _blowForce * Time.deltaTime);
-            }
+    private bool _isBlowing;
+
+    private InputController _input;
+
+    private void OnEnable() {
+        InputController[] inputs = FindObjectsByType<InputController>(FindObjectsSortMode.None);
+        if (inputs.Length != 0) {
+            _input = inputs[0];
         }
+
+        _input.Actions.Player.Attack.performed += StartBlowing;
+        _input.Actions.Player.Attack.canceled += StopBlowing;
+    }
+
+    private void OnDisable() {
+        _input.Actions.Player.Attack.performed -= StartBlowing;
+        _input.Actions.Player.Attack.canceled -= StopBlowing;
+    }
+
+    private void Update() {
+        if (!_isBlowing) {
+            return;
+        }
+
+        for (int i = 0; i < _bubblePool.AllBubbles.Count; i++) {
+            if (!IsWithinCone(_bubblePool.AllBubbles[i].transform.position)) {
+                continue;
+            }
+
+            _bubblePool.AllBubbles[i].Rigidbody.AddForce((_bubblePool.AllBubbles[i].transform.position - transform.position).normalized * _blowForce * Time.deltaTime);
+        }
+    }
+
+    private void StartBlowing(UnityEngine.InputSystem.InputAction.CallbackContext ctx) {
+        _isBlowing = true;
+        AudioSystem.Instance.PlaySound("Leaf Blower");
+    }
+
+    private void StopBlowing(UnityEngine.InputSystem.InputAction.CallbackContext ctx) {
+        _isBlowing = false;
+        AudioSystem.Instance.StopSound("Leaf Blower");
     }
 
     private bool IsWithinCone(Vector3 position) {
