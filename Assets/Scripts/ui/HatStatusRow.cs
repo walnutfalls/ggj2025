2477e869071f5f382b25e021ac0fd36d1bcd520d
ek <1350074+ek68794998@users.noreply.github.com>
2025-01-25 17:48:15 -0800

    Add tracker for hat statuses

---
+++ Assets/Scripts/ui/HatStatusRow.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HatStatusRow : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI nameText;
+
+    [SerializeField]
+    private Image iconImage;
+
+    [SerializeField]
+    private Sprite lockedIconSprite;
+
+    private HatScriptable hat;
+
+    private StatsTracker.HatStatus status;
+
+    protected void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void SetHat(HatScriptable hat)
+    {
+        this.hat = hat;
+        this.UpdateUiValues();
+    }
+
+    public void SetStatus(StatsTracker.HatStatus status)
+    {
+        this.status = status;
+        this.UpdateUiValues();
+    }
+
+    private void UpdateUiValues()
+    {
+        this.nameText.text = this.hat.Name.ToUpper();
+
+        Sprite rowSprite = this.status == StatsTracker.HatStatus.Locked
+            ? this.lockedIconSprite
+            : this.hat.HatSprite;
+        float rowOpacity = this.status == StatsTracker.HatStatus.UnlockedInCurrent
+            ? 1.0f
+            : 0.5f;
+
+        this.iconImage.sprite = rowSprite;
+
+        if (!Mathf.Approximately(rowOpacity, this.iconImage.color.a))
+        {
+            this.iconImage.color = new Color(1.0f, 1.0f, 1.0f, rowOpacity);
+        }
+    }
+}
